a55b3a84da3f57edafacaf3a1d4fdf7307819e3d
Abdul Arif <arifb@uwindsor.ca>
2021-10-18 17:39:27 -0400

    Added comments to scripts

--- Assets/Scripts/PickUpCounter.cs
+++ Assets/Scripts/PickUpCounter.cs
@@ -6,18 +6,6 @@ public class PickUpCounter : MonoBehaviour
 {
     public int points = 0;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnGUI()
     {
         GUI.color = Color.white;
--- Assets/Scripts/RespawnPoint.cs
+++ Assets/Scripts/RespawnPoint.cs
@@ -6,11 +6,15 @@ public class RespawnPoint : MonoBehaviour
 {
     public Transform Player;
     public Transform Respawn;
+    public Transform Object;
+    public Transform RespawnObject;
 
     void OnTriggerEnter(Collider other)
     {
-       if(other.CompareTag("Player"))
-        Player.transform.position = Respawn.transform.position;
-        Physics.SyncTransforms();
+       if(other.CompareTag("Player")){
+            Player.transform.position = Respawn.transform.position;
+            Object.transform.position = RespawnObject.transform.position;
+            Physics.SyncTransforms();
+       }
     }
 }
